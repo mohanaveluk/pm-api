aba96cd72928e54025eb2c8257488fc5d476ac7c
Mohanavelu Kumarasamy <mohanaveluk@gmail.com>
2026-09-17 18:23:26 -0500

    update vendor master fixes

--- src/database/migrations/1657774666972-init.ts
+++ src/database/migrations/1657774666972-init.ts
@@ -44,13 +44,15 @@ export class init1657774666972 implements MigrationInterface {
     //applications
     await queryRunner.query(`
           INSERT INTO roles (name, guid) VALUES 
-          ('Approver',         'b7488c81-1a2b-41b1-9f04-b101dc989075')
-          ,('Manager',          '31501ab9-d508-4aea-8046-f92327efe1b3')
-          ,('SuperAdmin',       '70ebd45b-b0b1-41cf-8e19-53caf36be1c3')
-          ,('OrganizationAdmin','ed12a0c4-0c16-4737-99d0-f15f6446f45d')
-          ,('User',             '921db096-14a2-424d-bb7c-afd9eaee3022')
-          ,('Vendor',           'd2a54247-3215-4e7c-8eda-754c314b2f33')
-          ,('Viewer',           '631dfd33-e742-4c01-b6c4-a52c3846d47d')
+          ('QAManager',                     'b7488c81-1a2b-41b1-9f04-b101dc989075')
+          ,('DepartmentManager',            '31501ab9-d508-4aea-8046-f92327efe1b3')
+          ,('SuperAdmin',                   '70ebd45b-b0b1-41cf-8e19-53caf36be1c3')
+          ,('OrganizationAdmin',            'ed12a0c4-0c16-4737-99d0-f15f6446f45d')
+          ,('DisciplineLead',               '921db096-14a2-424d-bb7c-afd9eaee3022')
+          ,('ProjectManager',               'd2a54247-3215-4e7c-8eda-754c314b2f33')
+          ,('ProcurementManager',           '631dfd33-e742-4c01-b6c4-a52c3846d47d')
+          ,('WarehouseManager',             '1fc3dbce-4282-4e24-81a1-381d191c0792')
+          ,('User',                         'e8f577d3-ad43-4563-8079-395fc4a7a94a')
           ;
           `);
     
